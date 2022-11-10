30de41c5d4aa0c1a3e8958c64d3643227fa67d52
kylesteinmann <105984447+kylesteinmann@users.noreply.github.com>
2022-11-09 23:35:45 -0600

    added classes and events

---
+++ src/app/Models/schedule.ts
@@ -0,0 +1,11 @@
+export class Schedule {
+  public date: string;
+  public eventName: string;
+
+  constructor(date: string, eventName: string) {
+    {
+      this.date = date;
+      this.eventName = eventName;
+    }
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatToolbarModule} from '@angular/material/toolbar';
-
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header/header.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './Components/home/home.component';
 import { AboutUsComponent } from './Components/about-us/about-us.component';
 import { MeetTheBoardComponent } from './Components/meet-the-board/meet-the-board.component';
 import { ScheduleAndClassesComponent } from './Components/schedule-and-classes/schedule-and-classes.component';
 import { ImportantInformationComponent } from './Components/important-information/important-information.component';
 import { SignUpAndPaymentComponent } from './Components/sign-up-and-payment/sign-up-and-payment.component';
-
 
 @NgModule({
   declarations: [
@@ -24,15 +23,16 @@ import { SignUpAndPaymentComponent } from './Components/sign-up-and-payment/sign
     MeetTheBoardComponent,
     ScheduleAndClassesComponent,
     ImportantInformationComponent,
-    SignUpAndPaymentComponent
+    SignUpAndPaymentComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatTableModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
