955d2224602521fcccbff3ccc30303c65adc5f81
kylesteinmann <105984447+kylesteinmann@users.noreply.github.com>
2022-11-14 18:36:46 -0600

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { HttpClientModule } from '@angular/common/http'
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 
 
 import { AppComponent } from './app.component';
