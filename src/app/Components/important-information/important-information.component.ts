9111790b44fe8307decf752f815963a65864a2d9
kylesteinmann <105984447+kylesteinmann@users.noreply.github.com>
2022-12-12 14:24:51 -0600

    small edit to Policy page and home page

--- src/app/Components/important-information/important-information.component.ts
+++ src/app/Components/important-information/important-information.component.ts
@@ -7,7 +7,7 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./important-information.component.css']
 })
 export class ImportantInformationComponent implements OnInit {
-  pdfSrc = '';
+  pdfSrc = 'https://firebasestorage.googleapis.com/v0/b/faith-family-homeschool-co-op.appspot.com/o/FFHC%20Handbook.pdf?alt=media&token=c2ce22a7-0b29-49d4-a189-a815f4f56d2e';
 
   cleanUrl() {
   const trustedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.pdfSrc);
